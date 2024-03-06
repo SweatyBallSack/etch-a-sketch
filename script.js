128e24ba02adb3349632eafd44cd17ffa5373869
Joseph Lin <149325305+SweatyBallSack@users.noreply.github.com>
2024-03-06 12:16:36 -0500

    Added an eventlistener that listens for mouseover events.
    This eventlistener is applied to the container div which houses all the divs that make up the grid.
    
    Once activated, the specific grid div would have its background color set to red through the classList.toggle method.
    
    Included a set timer within the eventlistener function that returns the background color to white after 500 ms.
    
    Currently the eventlistener is not working.

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 let container = document.querySelector("#container");
 
-
-
 for(let i = 1; i <= 16; i++) {
     let horizontalContainer = document.createElement('div');
     horizontalContainer.classList.add("horizontal-container" + i);
     horizontalContainer.setAttribute('style', 'display:flex; ');
     container.appendChild(horizontalContainer);
-
     for(let j = 1; j <= 16; j++) {
         let newDiv = document.createElement('div');
-        newDiv.classList.add("grid-box" + j);
-        newDiv.setAttribute('style', 'border: solid black; width: 500px; height: 5%; width: 5%; ');
+        newDiv.classList.add("grid-box-" + i + "-" + j); //ex: grid-box-10-2
+        newDiv.setAttribute('style', 'border: 1px solid black; width: 50px; height: 50px;');
         newDiv.textContent = i + " : " + j;
         let row = document.querySelector(".horizontal-container" + i);
         row.appendChild(newDiv);
-        
     }  
-    console.log("Row " + i + " finished.");
+    //console.log("Row " + i + " finished.");
 }
 
-// let div1 = document.createElement('div');
-// div1.setAttribute('style', 'border: solid black;');
-// div1.textContent = "div1";
-// let div2 = document.createElement('div');
-// div2.setAttribute('style', 'border: solid black;');
-// div2.textContent = "div2";
-// let div3 = document.createElement('div');
-// div3.setAttribute('style', 'border: solid black;');
-// div3.textContent = "div3";
+container.addEventListener('mouseover', (event) => {
+    let target = event.target;
 
-// container.appendChild(div1);
-// container.appendChild(div2);
-// container.appendChild(div3);
+    for(let i = 1; i <= 16; i++) {
+        // console.log(i);
+        for(let j = 1; j <= 16; j++) {
+            if(target.class == "grid-box-" + i + "-" + j) {
+                target.classList.toggle('active');
+                console.log(i + "-" + j + " grid-box reached.");
 
+                setTimeout(() => {
+                    target.classList.toggle('active');
+                }, 500);
+            }
+        }
+    }
+})
 
 
 
